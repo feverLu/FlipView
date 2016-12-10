d0fa9207df16bbc1f51dc3f1503d56f463af5980
Binbin Lu <sunday@Binbins-MacBook-Pro.local>
2016-12-09 23:11:04 -0800

    Initial code

---
+++ flipview/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using FlipViewDemo.Model;
+using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+
+namespace FlipViewDemo
+{
+    public sealed partial class MainPage : Page
+    {
+        public List<SampleItem> flipViewData { get; set; }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+        }
+
+        private void MainPage_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            flipViewData = new List<SampleItem>();
+            flipViewData.Add(new SampleItem() { Name = "Item 1", Image = "Assets/images/hotel.jpg" });
+            flipViewData.Add(new SampleItem() { Name = "Item 2", Image = "Assets/images/hotel (2).jpg" });
+            flipViewData.Add(new SampleItem() { Name = "Item 3", Image = "Assets/images/hotel (3).jpg" });
+            flipViewData.Add(new SampleItem() { Name = "Item 4", Image = "Assets/images/hotle.jpg" });
+            flipViewData.Add(new SampleItem() { Name = "Item 5", Image = "Assets/images/avalanche (2).jpg" });
+            flipView1.ItemsSource = flipViewData;
+        }
+    }
+}
